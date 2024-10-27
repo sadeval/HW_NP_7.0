6715a3631bd9f2daf2376db8df70276e5df3c623
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-27 19:39:54 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace HW_NP_7._0
+{
+    partial class Form1
+    {
+        // Объявление компонентов
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblPlayerTurn;
+        private System.Windows.Forms.PictureBox pbDice;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.RadioButton rbHumanVsHuman;
+        private System.Windows.Forms.RadioButton rbHumanVsComputer;
+        private System.Windows.Forms.Label lblPlayer1Score;
+        private System.Windows.Forms.Label lblPlayer2Score;
+
+        // Метод очистки используемых ресурсов.
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        // Инициализация компонентов формы
+        private void InitializeComponent()
+        {
+            this.lblPlayerTurn = new System.Windows.Forms.Label();
+            this.pbDice = new System.Windows.Forms.PictureBox();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.rbHumanVsHuman = new System.Windows.Forms.RadioButton();
+            this.rbHumanVsComputer = new System.Windows.Forms.RadioButton();
+            this.lblPlayer1Score = new System.Windows.Forms.Label();
+            this.lblPlayer2Score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPlayerTurn
+            // 
+            this.lblPlayerTurn.AutoSize = true;
+            this.lblPlayerTurn.Location = new System.Drawing.Point(12, 9);
+            this.lblPlayerTurn.Name = "lblPlayerTurn";
+            this.lblPlayerTurn.Size = new System.Drawing.Size(89, 16);
+            this.lblPlayerTurn.TabIndex = 0;
+            this.lblPlayerTurn.Text = "Ход игрока 1";
+            // 
+            // pbDice
+            // 
+            this.pbDice.Location = new System.Drawing.Point(15, 35);
+            this.pbDice.Name = "pbDice";
+            this.pbDice.Size = new System.Drawing.Size(154, 148);
+            this.pbDice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbDice.TabIndex = 1;
+            this.pbDice.TabStop = false;
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(15, 205);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(100, 23);
+            this.btnRoll.TabIndex = 2;
+            this.btnRoll.Text = "Бросить кость";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // rbHumanVsHuman
+            // 
+            this.rbHumanVsHuman.AutoSize = true;
+            this.rbHumanVsHuman.Checked = true;
+            this.rbHumanVsHuman.Location = new System.Drawing.Point(188, 35);
+            this.rbHumanVsHuman.Name = "rbHumanVsHuman";
+            this.rbHumanVsHuman.Size = new System.Drawing.Size(160, 20);
+            this.rbHumanVsHuman.TabIndex = 3;
+            this.rbHumanVsHuman.TabStop = true;
+            this.rbHumanVsHuman.Text = "Человек vs Человек";
+            this.rbHumanVsHuman.UseVisualStyleBackColor = true;
+            this.rbHumanVsHuman.CheckedChanged += new System.EventHandler(this.rbHumanVsHuman_CheckedChanged);
+            // 
+            // rbHumanVsComputer
+            // 
+            this.rbHumanVsComputer.AutoSize = true;
+            this.rbHumanVsComputer.Location = new System.Drawing.Point(188, 61);
+            this.rbHumanVsComputer.Name = "rbHumanVsComputer";
+            this.rbHumanVsComputer.Size = new System.Drawing.Size(177, 20);
+            this.rbHumanVsComputer.TabIndex = 4;
+            this.rbHumanVsComputer.Text = "Человек vs Компьютер";
+            this.rbHumanVsComputer.UseVisualStyleBackColor = true;
+            this.rbHumanVsComputer.CheckedChanged += new System.EventHandler(this.rbHumanVsComputer_CheckedChanged);
+            // 
+            // lblPlayer1Score
+            // 
+            this.lblPlayer1Score.AutoSize = true;
+            this.lblPlayer1Score.Location = new System.Drawing.Point(185, 97);
+            this.lblPlayer1Score.Name = "lblPlayer1Score";
+            this.lblPlayer1Score.Size = new System.Drawing.Size(110, 16);
+            this.lblPlayer1Score.TabIndex = 5;
+            this.lblPlayer1Score.Text = "Счет игрока 1: 0";
+            // 
+            // lblPlayer2Score
+            // 
+            this.lblPlayer2Score.AutoSize = true;
+            this.lblPlayer2Score.Location = new System.Drawing.Point(185, 130);
+            this.lblPlayer2Score.Name = "lblPlayer2Score";
+            this.lblPlayer2Score.Size = new System.Drawing.Size(110, 16);
+            this.lblPlayer2Score.TabIndex = 6;
+            this.lblPlayer2Score.Text = "Счет игрока 2: 0";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(374, 240);
+            this.Controls.Add(this.lblPlayer2Score);
+            this.Controls.Add(this.lblPlayer1Score);
+            this.Controls.Add(this.rbHumanVsComputer);
+            this.Controls.Add(this.rbHumanVsHuman);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.pbDice);
+            this.Controls.Add(this.lblPlayerTurn);
+            this.Name = "Form1";
+            this.Text = "Игра в Кости";
+            ((System.ComponentModel.ISupportInitialize)(this.pbDice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace HW_NP_7._0
+{
+    public partial class Form1 : Form
+    {
+        private RandomOrgApi randomOrgApi;
+        private bool isPlayerOneTurn = true;
+        private int player1Score = 0;
+        private int player2Score = 0;
+        private GameMode gameMode;
+
+        public Form1()
+        {
+            InitializeComponent();
+            randomOrgApi = new RandomOrgApi();
+            gameMode = GameMode.HumanVsHuman;
+            UpdateTurnIndicator();
+        }
+
+        private async void btnRoll_Click(object sender, EventArgs e)
+        {
+            btnRoll.Enabled = false;
+
+            await AnimateDiceRoll();
+
+            int diceValue = await randomOrgApi.RollDice();
+            UpdateDiceImage(diceValue);
+
+            UpdateScore(diceValue);
+
+            if (gameMode == GameMode.HumanVsHuman)
+            {
+                isPlayerOneTurn = !isPlayerOneTurn;
+                UpdateTurnIndicator();
+            }
+            else if (gameMode == GameMode.HumanVsComputer)
+            {
+                await Task.Delay(500); // Пауза перед ходом компьютера
+                await ComputerTurn();
+            }
+
+            btnRoll.Enabled = true;
+        }
+
+        private async Task ComputerTurn()
+        {
+            lblPlayerTurn.Text = "Ход компьютера";
+
+            await AnimateDiceRoll();
+
+            int diceValue = await randomOrgApi.RollDice();
+            UpdateDiceImage(diceValue);
+
+            player2Score += diceValue;
+            lblPlayer2Score.Text = $"Счет компьютера: {player2Score}";
+
+            lblPlayerTurn.Text = "Ваш ход";
+        }
+
+        private void UpdateScore(int diceValue)
+        {
+            if (isPlayerOneTurn)
+            {
+                player1Score += diceValue;
+                lblPlayer1Score.Text = $"Счет игрока 1: {player1Score}";
+            }
+            else
+            {
+                player2Score += diceValue;
+                if (gameMode == GameMode.HumanVsHuman)
+                {
+                    lblPlayer2Score.Text = $"Счет игрока 2: {player2Score}";
+                }
+                else
+                {
+                    lblPlayer2Score.Text = $"Счет компьютера: {player2Score}";
+                }
+            }
+        }
+
+        private void UpdateTurnIndicator()
+        {
+            if (gameMode == GameMode.HumanVsHuman)
+            {
+                lblPlayerTurn.Text = isPlayerOneTurn ? "Ходит игрок 1" : "Ходит игрок 2";
+            }
+            else if (gameMode == GameMode.HumanVsComputer)
+            {
+                lblPlayerTurn.Text = "Ваш ход";
+            }
+        }
+
+        private void UpdateDiceImage(int diceValue)
+        {
+            switch (diceValue)
+            {
+                case 1:
+                    pbDice.Image = Properties.Resources.dice_1;
+                    break;
+                case 2:
+                    pbDice.Image = Properties.Resources.dice_2;
+                    break;
+                case 3:
+                    pbDice.Image = Properties.Resources.dice_3;
+                    break;
+                case 4:
+                    pbDice.Image = Properties.Resources.dice_4;
+                    break;
+                case 5:
+                    pbDice.Image = Properties.Resources.dice_5;
+                    break;
+                case 6:
+                    pbDice.Image = Properties.Resources.dice_6;
+                    break;
+                default:
+                    pbDice.Image = null;
+                    break;
+            }
+        }
+
+        private async Task AnimateDiceRoll()
+        {
+            int animationSteps = 10;
+            int delay = 50;
+
+            Random rnd = new Random();
+            for (int i = 0; i < animationSteps; i++)
+            {
+                int randomValue = rnd.Next(1, 7);
+                UpdateDiceImage(randomValue);
+                await Task.Delay(delay);
+            }
+        }
+
+        private void rbHumanVsHuman_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbHumanVsHuman.Checked)
+            {
+                gameMode = GameMode.HumanVsHuman;
+                ResetGame();
+            }
+        }
+
+        private void rbHumanVsComputer_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbHumanVsComputer.Checked)
+            {
+                gameMode = GameMode.HumanVsComputer;
+                ResetGame();
+            }
+        }
+
+        private void ResetGame()
+        {
+            isPlayerOneTurn = true;
+            player1Score = 0;
+            player2Score = 0;
+            lblPlayer1Score.Text = "Счет игрока 1: 0";
+            lblPlayer2Score.Text = gameMode == GameMode.HumanVsHuman ? "Счет игрока 2: 0" : "Счет компьютера: 0";
+            UpdateTurnIndicator();
+            pbDice.Image = null;
+        }
+    }
+
+    public enum GameMode
+    {
+        HumanVsHuman,
+        HumanVsComputer
+    }
+
+    public class RandomOrgApi
+    {
+        private readonly string apiKey = "db46b969-8397-4358-8f22-24245f2b011e";
+        private readonly string apiUrl = "https://api.random.org/json-rpc/4/invoke";
+
+        public async Task<int> RollDice()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var request = new
+                {
+                    jsonrpc = "2.0",
+                    method = "generateIntegers",
+                    @params = new
+                    {
+                        apiKey = apiKey,
+                        n = 1,
+                        min = 1,
+                        max = 6,
+                        replacement = true
+                    },
+                    id = 1
+                };
+
+                var jsonRequest = JsonConvert.SerializeObject(request);
+                var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(apiUrl, content);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                var resultObject = JsonConvert.DeserializeObject<RandomOrgResponse>(jsonResponse);
+
+                return resultObject.result.random.data[0];
+            }
+        }
+    }
+
+    public class RandomOrgResponse
+    {
+        public string jsonrpc { get; set; }
+        public RandomOrgResult result { get; set; }
+        public int id { get; set; }
+    }
+
+    public class RandomOrgResult
+    {
+        public RandomData random { get; set; }
+        public int bitsUsed { get; set; }
+        public int bitsLeft { get; set; }
+        public int requestsLeft { get; set; }
+        public int advisoryDelay { get; set; }
+    }
+
+    public class RandomData
+    {
+        public List<int> data { get; set; }
+        public string completionTime { get; set; }
+    }
+}
